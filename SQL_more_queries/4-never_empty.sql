effb5acf1aa4586faadedf868c6bde83bba6e6be
fmhirwa <m.hirwa@alustudent.com>
2023-03-31 12:17:49 +0200

    I can finally rest...

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Same but different
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+id INT DEFAULT 1,
+name VARCHAR(256)
+);
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Joins from left
+SELECT cities.id, cities.name, states.name
+FROM cities LEFT JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
