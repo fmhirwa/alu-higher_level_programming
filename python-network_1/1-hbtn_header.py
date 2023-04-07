8e56787868c02ca8efaed4158e40e430a9a5d9e7
fmhirwa <m.hirwa@alustudent.com>
2023-04-07 20:36:25 +0200

    Document

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,7 +5,6 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
-"""Documented"""
     with urllib.request.urlopen(sys.argv[1]) as response:
         headers = response.info()
         print(headers.get("X-Request-Id"))
