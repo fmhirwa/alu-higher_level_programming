c492209d46e07813fa6cec316c022c184bb70acb
fmhirwa <m.hirwa@alustudent.com>
2023-04-07 20:46:15 +0200

    Losing it

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -7,9 +7,9 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
+    token = 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
     url = "https://api.github.com/user"
-    r = requests.get(url, auth=(username,
-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))
+    r = requests.get(url, auth=(username, token))
     try:
         print(r.json().get("id"))
     except ValueError:
