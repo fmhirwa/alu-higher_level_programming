f1bb69cde958a695a8b76d8742c5be5d58f89e90
fmhirwa <m.hirwa@alustudent.com>
2023-04-26 11:25:15 +0200

    Pushy pushy

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""Documented module"""
+
+
+def text_indentation(text):
+    """ Indent function """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
