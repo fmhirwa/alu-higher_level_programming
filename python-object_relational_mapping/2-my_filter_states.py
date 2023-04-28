7ae3a950f61a7fe700c5b36d1447922e84b5cfae
fmhirwa <m.hirwa@alustudent.com>
2023-04-28 13:12:10 +0200

    Doc fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-""" This is a script that lists all states from the database hbtn_0e_0_usa"""
-from sys import argv
-import MySQLdb
+""" This is proper documentation """
 
-""" This is a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+def select_states():
+    """ lists all states that matches arvg[4]
+            from the database
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cur = db.cursor()
+
+    sql = "SELECT * FROM states\
+        WHERE BINARY name='{:s}' ORDER BY id"\
+        .format(sys.argv[4])
+
+    cur.execute(sql)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-""" Documented """
-    user, passwd, db, argument = argv[1:]
-
-    db_setting = {
-            "host": "localhost",
-            "port": 3306,
-            "user": user,
-            "passwd": passwd,
-            "db": db,
-            "charset": "utf8"
-            }
-
-    conn = MySQLdb.connect(**db_setting)
-    cur = conn.cursor()
-
-    query = "SELECT * FROM states WHERE name LIKE BINARY "{}" ORDER BY id"\
-            .format(argument)
-
-    cur.execute(query)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    select_states()
