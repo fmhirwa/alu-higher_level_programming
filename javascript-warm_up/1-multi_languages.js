15397e97d1caee626c9a589abf3599b4a46b61dd
fmhirwa <m.hirwa@alustudent.com>
2023-06-16 19:37:51 +0200

    At least 90s

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun
-Python is cool
-JavaScript is amazing');
+
+const lines = ['JavaScript is amazing', 'Python is cool', 'C is fun'];
+for (const line of lines.reverse()) console.log(line);
